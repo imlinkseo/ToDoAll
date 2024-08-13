dde203df9f2198abba010a37145516fce1abd7fb
imlinkseo <152132698+imlinkseo@users.noreply.github.com>
2024-08-13 11:37:04 +0900

    [ADD] modecontrol store link

--- todoall/src/components/button/index.tsx
+++ todoall/src/components/button/index.tsx
@@ -2,45 +2,45 @@ import React from "react";
 import styled from "styled-components";
 
 interface Ibutton {
-  children: React.ReactNode;
-  color: string;
-  size: string;
-  event?: () => void;
+	children: React.ReactNode;
+	color: string;
+	size: string;
+	event?: () => void;
 }
 
 const StyledButton = styled.button<{ color: string; size: string }>`
-  display: flex;
-  gap: 16px;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  background-color: ${(props) =>
-    props.color
-      ? props.color === "dark"
-        ? "var(--mono-black)"
-        : "var(--bg-gray)"
-      : "var(--mono-black)"};
-  color: ${(props) =>
-    props.color
-      ? props.color === "dark"
-        ? "var(--mono-white)"
-        : "var(--mono-black)"
-      : "var(--mono-white)"};
-  padding: ${(props) =>
-    props.size
-      ? props.size === "big"
-        ? "var(--base-unit) calc(var(--base-unit) * 2)"
-        : "var(--base-unit)"
-      : "var(--base-unit) calc(var(--base-unit) * 2)"};
-  border-radius: 20px;
+	display: flex;
+	gap: 16px;
+	justify-content: center;
+	align-items: center;
+	border: none;
+	background-color: ${(props) =>
+		props.color
+			? props.color === "dark"
+				? "var(--mono-black)"
+				: "var(--bg-gray)"
+			: "var(--mono-black)"};
+	color: ${(props) =>
+		props.color
+			? props.color === "dark"
+				? "var(--mono-white)"
+				: "var(--mono-black)"
+			: "var(--mono-white)"};
+	padding: ${(props) =>
+		props.size
+			? props.size === "big"
+				? "var(--base-unit) calc(var(--base-unit) * 2)"
+				: "var(--base-unit)"
+			: "var(--base-unit) calc(var(--base-unit) * 2)"};
+	border-radius: 20px;
 `;
 
 function Button({ color, size, children, event }: Ibutton): JSX.Element {
-  return (
-    <StyledButton color={color} size={size} onClick={event} className="pointer">
-      {children}
-    </StyledButton>
-  );
+	return (
+		<StyledButton color={color} size={size} onClick={event} className="pointer">
+			{children}
+		</StyledButton>
+	);
 }
 
 export { Button };
--- todoall/src/components/modeControl/index.tsx
+++ todoall/src/components/modeControl/index.tsx
@@ -1,96 +1,120 @@
 import React from "react";
 import styled from "styled-components";
+import { Tmode } from "src/types/types";
+import { useControlStore } from "@stores/modeStore";
 
 const StyledModeButtonBG = styled.span`
-  z-index: 0;
-  position: absolute;
-  width: 100px;
-  height: 36px;
-  background-color: var(--rgba-deep-gray);
-  border-radius: 20px;
-  top: 50%;
-  left: 8px;
-  transform: translateY(-50%);
-  transition: left 0.4s ease-out;
+	z-index: 0;
+	position: absolute;
+	width: 100px;
+	height: 36px;
+	background-color: var(--rgba-deep-gray);
+	border-radius: 20px;
+	top: 50%;
+	left: 8px;
+	transform: translateY(-50%);
+	transition: left 0.8s ease-out;
 `;
 
 function ModeButtonBG() {
-  return (
-    <>
-      <StyledModeButtonBG className="modelButtonBG"></StyledModeButtonBG>
-    </>
-  );
+	return (
+		<>
+			<StyledModeButtonBG className="modelButtonBG"></StyledModeButtonBG>
+		</>
+	);
 }
-
-let bg: HTMLSpanElement | null = document.querySelector(".modelButtonBG");
 
 interface ImodeButton {
-  children: React.ReactNode;
-  event: (e: React.MouseEvent) => void;
-}
-
-function getXY(e: React.MouseEvent) {
-  if (bg && e.currentTarget) {
-    bg.style.left = `${(e.currentTarget as HTMLButtonElement).offsetLeft}px`;
-  }
-  return;
+	children: React.ReactNode;
+	mode: Tmode;
+	event: (e: React.MouseEvent) => void;
 }
 
 const StyledModeButton = styled.button`
-  z-index: 2;
-  width: 100px;
-  height: 36px;
-  border: none;
-  background-color: transparent;
-  padding: 8px 16px;
-  border-radius: 20px;
-  transition: background-color 0.3s ease;
-  &:hover {
-    background-color: var(--rgba-light-gray);
-  }
+	z-index: 2;
+	width: 100px;
+	height: 36px;
+	border: none;
+	background-color: transparent;
+	padding: 8px 16px;
+	border-radius: 20px;
+	&:hover {
+		background-color: var(--rgba-light-gray);
+	}
 `;
 
-function ModeButton({ children, event }: ImodeButton) {
-  return (
-    <>
-      <StyledModeButton className="pointer" onClick={event}>
-        {children}
-      </StyledModeButton>
-    </>
-  );
+function ModeButton({ children, mode, event }: ImodeButton) {
+	return (
+		<>
+			<StyledModeButton
+				className="pointer modeBtn"
+				onClick={event}
+				data-mode={mode}
+			>
+				{children}
+			</StyledModeButton>
+		</>
+	);
 }
 
 const StyledModeControl = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  padding: 8px;
-  box-sizing: border-box;
-  background-color: var(--rgba-light-gray);
-  border-radius: 40px;
+	position: relative;
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	padding: 8px;
+	box-sizing: border-box;
+	background-color: var(--rgba-light-gray);
+	border-radius: 40px;
 `;
 
 function ModeControl() {
-  return (
-    <>
-      <StyledModeControl>
-        <ModeButton event={getXY}>
-          <span className="capitalize">all</span>
-        </ModeButton>
-        <ModeButton event={getXY}>
-          <span className="capitalize">board</span>
-        </ModeButton>
-        <ModeButton event={getXY}>
-          <span className="capitalize">calendar</span>
-        </ModeButton>
-        <ModeButton event={getXY}>
-          <span className="capitalize">list</span>
-        </ModeButton>
-        <ModeButtonBG />
-      </StyledModeControl>
-    </>
-  );
+	const { setMode } = useControlStore();
+
+	function getXYnMode(e: React.MouseEvent) {
+		let bg: HTMLSpanElement | null = document.querySelector(".modelButtonBG");
+		let modeBtns: NodeListOf<Element> | null =
+			document.querySelectorAll(".modeBtn");
+		if (bg && e.currentTarget && modeBtns) {
+			modeBtns.forEach((modeBtn) => {
+				modeBtn.classList.remove("selected");
+			});
+			e.currentTarget.classList.add("selected");
+			bg.style.left = `${(e.currentTarget as HTMLButtonElement).offsetLeft}px`;
+			let mode = (e.currentTarget as HTMLButtonElement).dataset.mode;
+			setMode(mode as Tmode);
+		}
+		return;
+	}
+
+	return (
+		<>
+			<StyledModeControl>
+				<ModeButton event={getXYnMode} mode={"all"}>
+					<span className="capitalize">all</span>
+				</ModeButton>
+				<ModeButton event={getXYnMode} mode={"board"}>
+					<span className="capitalize">board</span>
+				</ModeButton>
+				<ModeButton event={getXYnMode} mode={"calendar"}>
+					<span className="capitalize">calendar</span>
+				</ModeButton>
+				<ModeButton event={getXYnMode} mode={"list"}>
+					<span className="capitalize">list</span>
+				</ModeButton>
+				<ModeButtonBG />
+			</StyledModeControl>
+		</>
+	);
 }
+
+window.addEventListener("resize", () => {
+	let bg: HTMLSpanElement | null = document.querySelector(".modelButtonBG");
+	let selectedModeBtn: HTMLButtonElement | null =
+		document.querySelector(".modeBtn.selected");
+	if (bg && selectedModeBtn) {
+		bg.style.left = `${selectedModeBtn.offsetLeft}px`;
+	}
+});
 
 export { ModeControl };
